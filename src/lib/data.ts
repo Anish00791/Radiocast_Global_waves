193201928a686be8f7a6e0817c86fc7619d5aca5
Anish Mohanty <147069047+Anish00791@users.noreply.github.com>
2025-06-17 17:30:32 +0530

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -8,7 +8,7 @@ export const radioStations: RadioStation[] = [
     country: "United Kingdom",
     language: "English",
     genre: ["Pop", "Top 40", "Chart"],
-    url: "http://a.files.bbci.co.uk/ms6/live/3441A116-B12E-4D2F-ACA8-C1984642FA4B/audio/simulcast/hls/nonuk/pc_hd_abr_v2/ak/bbc_radio_one.m3u8",
+    url: "https://a.files.bbci.co.uk/ms6/live/3441A116-B12E-4D2F-ACA8-C1984642FA4B/audio/simulcast/hls/nonuk/pc_hd_abr_v2/ak/bbc_radio_one.m3u8",
     favicon: "	https://cdn-radiotime-logos.tunein.com/s24939q.png",
     mood: ["Energetic", "Popular"],
     featured: true,
